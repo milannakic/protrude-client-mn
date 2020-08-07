89a21b4525272d1ed8b30914cfe21df6d9856a2b
Milan Nakic <milan.nakic@gmail.com>
2020-08-07 23:14:46 +0200

    Styled Navbar and added pictures to be used for logo etc

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,24 +1,28 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import Logo from "../images/protrude-logo.png";
 
 class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand">
         <div className="container-fluid">
-          <Link to="/" className="navbar-brand">
-            <img src="" alt="Protrude Home"></img>
-          </Link>
+          <div className="navbar-header">
+            <Link to="/" className="navbar-brand">
+              <img src={Logo} alt="Protrude Home"></img>
+            </Link>
+          </div>
+
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <Link to="/signup">Sign Up!</Link>
+            </li>
+            <li>
+              <Link to="/login">Log in</Link>
+            </li>
+          </ul>
         </div>
-        <ul className="nav navbar-nav navbar-right">
-          <li>
-            <Link to="/signup">Sign Up!</Link>
-          </li>
-          <li>
-            <Link to="/login">Log in</Link>
-          </li>
-        </ul>
       </nav>
     );
   }
