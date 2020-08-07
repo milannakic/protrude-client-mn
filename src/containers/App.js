4551c2e0825c5a772596cff075a84814769782f9
Milan Nakic <milan.nakic@gmail.com>
2020-08-07 22:04:03 +0200

    Added Navbar component

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Provider } from "react-redux";
 import { configureStore } from "../store";
 import { BrowserRouter as Router } from "react-router-dom";
-//import Navbar from "./Navbar";
+import Navbar from "./Navbar";
 //import Main from "./Main";
 
 const store = configureStore();
@@ -10,7 +10,9 @@ const store = configureStore();
 const App = () => (
   <Provider store={store}>
     <Router>
-      <div>Hello world</div>
+      <div className="onboarding">
+        <Navbar />
+      </div>
     </Router>
   </Provider>
 );
