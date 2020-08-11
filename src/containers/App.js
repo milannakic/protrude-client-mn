41d24b9b161a6467ad36028e22d9a3dd598d74ee
Milan Nakic <milan.nakic@gmail.com>
2020-08-11 18:38:33 +0200

    Added JWT to HTTP Headers

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,8 +4,21 @@ import { configureStore } from "../store";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./Navbar";
 import Main from "./Main";
+import { setAuthorizationToken, setCurrentUser } from "../store/actions/auth";
+import jwtDecode from "jwt-decode";
 
 const store = configureStore();
+
+if (localStorage.jwtToken) {
+  setAuthorizationToken(localStorage.jwtToken);
+  //this repopulates the redux state with the token from localStorage in case of refresh
+  //prevent manually tempering with the jwtToken key in localStorage
+  try {
+    store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)));
+  } catch (err) {
+    store.dispatch(setCurrentUser({}));
+  }
+}
 
 const App = () => (
   <Provider store={store}>
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,14 @@
 import axios from "axios";
 
+export function setTokenHeader(token) {
+  //when we call this function and pass a token to it, it will be attached to all our future requests
+  if (token) {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete axios.defaults.headers.common["Authorization"];
+  }
+}
+
 export function apiCall(method, path, data) {
   return new Promise((resolve, reject) => {
     return axios[method](path, data)
