469ce87a4c675ac46862a771662c9d80a36dae86
Milan Nakic <milan.nakic@gmail.com>
2020-08-11 18:01:04 +0200

    Added logout option and adjusted navbar logic

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Homepage = () => {
+const Homepage = ({ currentUser }) => {
+  if (!currentUser.isAuthenticated) {
+    return (
+      <div className="protrude-home">
+        <h1>Well hello there!</h1>
+        <h4>New to Protrude?</h4>
+        <Link to="/signup" className="btn btn-primary">
+          Sign up here
+        </Link>
+      </div>
+    );
+  }
   return (
-    <div className="protrude-home">
-      <h1>Well hello there!</h1>
-      <h4>New to Protrude?</h4>
-      <Link to="/signup" className="btn btn-primary">
-        Sign up here
-      </Link>
+    <div>
+      <h4>YOU MADE IT!</h4>
     </div>
   );
 };
